3edbce7ebaf1857b96d587ed4773cfe31e9c0c3e
thomasgalliker <thomas@bluewin.ch>
2016-09-15 07:56:21 +0200

    Update AssemblyInfos

--- CrossPlatformLibrary.Settings.Android/Properties/AssemblyInfo.cs
+++ CrossPlatformLibrary.Settings.Android/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -25,6 +23,5 @@ using Android.App;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.1")]
+[assembly: AssemblyFileVersion("2.0.1")]
