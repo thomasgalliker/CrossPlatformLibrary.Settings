a497c75d1dab3e70519d51df3457e1cfac032370
thomasgalliker <thomas@bluewin.ch>
2016-12-08 21:22:10 +0100

    Release version 2.0.1-pre8

--- CrossPlatformLibrary.Settings/Properties/AssemblyInfo.cs
+++ CrossPlatformLibrary.Settings/Properties/AssemblyInfo.cs
@@ -24,4 +24,4 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("2.0.1")]
-[assembly: AssemblyFileVersion("2.0.1-pre6")]
+[assembly: AssemblyFileVersion("2.0.1-pre7")]
